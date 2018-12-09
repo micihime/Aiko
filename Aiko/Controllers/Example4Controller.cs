55ff9eb95f23e7a966f167e9c19aff2397fa8aaf
Miči <miroslava.hreskova@student.tuke.sk>
2018-12-09 17:00:44 +0100

    action filters
    - created custom action filter

---
+++ Aiko/Controllers/Example4Controller.cs
@@ -0,0 +1,35 @@
+﻿using Aiko.Filters;
+using System.Web.Mvc;
+
+namespace Aiko.Controllers
+{
+    //Action Filter
+    // - apply pre and post processing logic to a controller action and its result
+    // - used to apply cross-cutting logic - logic that has to execute across multiple controller actions but you don't want to duplicate the code inside of 
+    //each individual controller
+    // - can be applied to action, controller or to whole app (in Global.asax.cs -> FilterConfig.cs)
+
+    //OutputCache
+    // - tells the run time that it's allowed to cash the final output of some action and to use that cashed result to service future requests
+    // - can dramatically increase the throughput and scalability of an application
+    //ValidateInput
+    // - turn off request validation and allow dangerous input
+    //Authorize
+    // - ensures that a user is logged in, perhaps in a specific role, before the action is allowed to process that user's request
+    //ValidateAntiForgeryToken
+    // - helps prevent cross site request forgeries
+    //HandleError
+    // - can specify a view to render in the event of an unhandled exception
+    public class Example4Controller : Controller
+    {
+        // GET: Example4
+        [Log]
+        //URL to the browser -> OnActionExecuting -> action method -> OnActionExecuted -> View is rendered -> OnResultExecuted
+        public ActionResult Index()
+        {
+            int a = 5 * 5;
+            //throw new Exception();
+            return View();
+        }
+    }
+}
---
+++ Aiko/Filters/LogAttribute.cs
@@ -0,0 +1,31 @@
+﻿using System.Web.Mvc;
+
+namespace Aiko.Filters
+{
+    public class LogAttribute : ActionFilterAttribute
+    {
+        //Called by the ASP.NET MVC framework before the action method executes
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            base.OnActionExecuting(filterContext);
+        }
+
+        //Called by the ASP.NET MVC framework after the action method executes
+        public override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            base.OnActionExecuted(filterContext);
+        }
+
+        //Called by the ASP.NET MVC framework before the action result executes (for instance before a view renders)
+        public override void OnResultExecuting(ResultExecutingContext filterContext)
+        {
+            base.OnResultExecuting(filterContext);
+        }
+
+        //Called by the ASP.NET MVC framework after the action result executes
+        public override void OnResultExecuted(ResultExecutedContext filterContext)
+        {
+            base.OnResultExecuted(filterContext);
+        }
+    }
+}
