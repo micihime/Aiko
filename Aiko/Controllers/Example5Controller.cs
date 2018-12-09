ea7d43ea6e3862b8fc29157e6ee2d4a11503cf73
Miči <miroslava.hreskova@student.tuke.sk>
2018-12-09 17:11:05 +0100

    views and models
    - create a model
    - create a view to display the model

---
+++ Aiko/Controllers/Example5Controller.cs
@@ -0,0 +1,24 @@
+﻿using Aiko.Models;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace Aiko.Controllers
+{
+    public class Example5Controller : Controller
+    {
+        private List<Example> exampleList = new List<Example>
+        {
+            new Example { Name = "Zlatovlaska", City = "Za siedmymi horami", Country = "Neverland", Rating = 5 },
+            new Example { Name = "Sipova Ruzenka", City = "Za siedmymi dolami", Country = "Neverland", Rating = 4 },
+            new Example { Name = "Kocur v cizmach", City = "Kde sa voda sypala", Country = "Neverland", Rating = 10 },
+            new Example { Name = "Ententiky", City = "A piesok sa lial", Country = "Neverland", Rating = 7 },
+            new Example { Name = "2 spendliky", City = "<script>alert('xss');</script>", Country = "Atlantida", Rating = 1 } //Razor HTML encodes data by default
+        };
+
+        // GET: Example5
+        public ActionResult Index()
+        {
+            return View(exampleList);
+        }
+    }
+}
---
+++ Aiko/Models/Example.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Aiko.Models
+{
+    public class Example
+    {
+        public string Name { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public int Rating { get; set; }
+    }
+}
