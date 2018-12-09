92b988492d3a610ea0e3e34a4312508eaa11c39d
Miči <miroslava.hreskova@student.tuke.sk>
2018-12-09 16:15:37 +0100

    routes and actions
    - adding new route
    - adding new controller with search action that accepts string input

--- Aiko/App_Start/RouteConfig.cs
+++ Aiko/App_Start/RouteConfig.cs
@@ -9,10 +9,25 @@ namespace Aiko
 {
     public class RouteConfig
     {
+        //routing engine
+        // - core part of asp .net (not tied to mvc fw)
+        // - directs requests to controllers by following a map (a pattern it can use and apply to find out where to send the request
+        //and to find parameters in the URL)
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            //map allows (for the route)
+            // - providing a friendly name
+            // - a pattern
+            // - default parameters
+
+            routes.MapRoute( //adds entry to the routing table
+                name: "Cuisine",
+                url: "Cuisine/{action}/{name}",
+                defaults: new { controller = "Example1", action = "Search", name = UrlParameter.Optional }
+            );
+            //the first route that matches the URL will "win"
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
---
+++ Aiko/Controllers/Example1Controller.cs
@@ -0,0 +1,23 @@
+﻿using System.Web.Mvc;
+
+namespace Aiko.Controllers
+{
+    //every public method is URL-addressable (depending on routing settings)
+    public class Example1Controller : Controller
+    {
+        // GET: Cuisine
+        // MVC fw is looking into different places to search for the input parameter (inspectiong route data, query string and posted form values)
+        // - I can get here by /Cuisine/french or also by query string Cuisine?name=french
+        public ActionResult Search(string name)
+        {
+            //you never directly inspect route data or query string - it is taken care of by MVC fw 
+            //RouteData.Values
+            //Request.QueryString
+
+            //encoding value because we are returning string content - if the user snuck through some malicious sctipt tag or something
+            var message = Server.HtmlEncode(name); //it will render as text and it will prevent a cross-site scripting attack (Razor engine does this automatically)
+
+            return Content(message); //string literal
+        }
+    }
+}
